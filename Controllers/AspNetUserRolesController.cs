4aeedd0f43af2dc6dcb82f7cd05d53b5cd6c3a3d
aidenwittnz <aidenwitt.nz@gmail.com>
2023-04-23 19:36:40 -0500

    AspNetUserRoles implementation

--- Controllers/AspNetUserRolesController.cs
+++ Controllers/AspNetUserRolesController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Capstone.Models;
+using CapstoneProject.Data;
+using CapstoneProject.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Drawing.Printing;
+using System.Data;
 
 namespace CapstoneProject.Controllers
 {
     public class AspNetUserRoles : Controller
     {
-        public IActionResult Index()
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AspNetUserRoles(RoleManager<IdentityRole> roleManager)
         {
-            return View();
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
         }
     }
 }
